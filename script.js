ab60b14cc3e06fb598c22fcc80bff57fde09ce0f
Bala1604 <balasm1504@gmail.com>
2026-07-17 11:50:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,144 @@
+//  Task 1. Employee Registration
+const companyName = "Stackly";
+let empId = "emp100";
+let empName= prompt("Enter Emp Name");
+
+console.log("companyName:",companyName);
+console.log("empID:",empId);
+console.log("empName:",empName);
+
+// Task 2: College Admission
+const clgName = "Paavai Institutions";
+let stdName = prompt("Enter the Std Name");
+let course = prompt("enter the Course Name");
+
+console.log("clgName:", clgName);
+console.log("stdName", stdName);
+console.log("course:", course);
+
+// Task 3: Food Order
+const RestaurantName = "Zomoto";
+let  CustName = prompt("Enter the customer name");
+let foodName = prompt("Enter the food name");
+let quanity = 2;
+
+console.log("RestaurantName:", RestaurantName);
+console.log("CustName:",CustName);
+console.log("foodName:",foodName);
+console.log("quanity:",quanity);
+
+// Task 4: Hospital Registration
+const HpName = "GH";
+let patientName = prompt("enter the patient Name");
+let tokenNumber = 30;
+
+console.log("HpName:",HpName);
+console.log("patientName:",patientName);
+console.log("tokenNumber:",tokenNumber);
+
+// Task 5: Movie Ticket Booking
+const theatrName = "LA Cinema";
+let movieName = prompt("enter movie name");
+let customerName = prompt("enter customer name");
+let seatNumber = "A17";
+
+console.log("theatreName:",theatrName);
+console.log("movieName:",movieName);
+console.log("customerName:",customerName);
+console.log("seatNumber:",seatNumber);
+
+// Task 6: Mobile Shop
+const shopName = "W2 Mobile";
+let custName = prompt("enter the customer name");
+let mblModel = prompt("enter the mobile model");
+let price=15000;
+
+console.log("shopName:",shopName);
+console.log("custName:",custName);
+console.log("mblModel:",mblModel);
+console.log("price:",price);
+
+
+// Task 7: Gym Membership
+const GymName = "GOT";
+let memberName = prompt("enter the member name");
+let mshipFee = 5000;
+
+console.log("gymName:",GymName);
+console.log("memberName:",memberName);
+console.log("mshipFee:",mshipFee);
+
+
+// Task 8: ATM Welcome Screen
+const BankName = "SBI";
+let UserName = prompt("Enter The User Name");
+
+console.log("BankName :", BankName);
+console.log("UserName :", UserName);
+
+//Task 9: Bus Ticket
+const TravelsName ="Zing";
+let PassengerName = prompt("Enter The Passenger Name");
+let SeatNumber = 22;
+
+console.log("TravelsName :",TravelsName);
+console.log("PassengerName :",PassengerName);
+console.log("SeatNumber :",SeatNumber);
+
+
+//Task 10: Online Shopping
+const WebSideNAme ="Meesho";
+let ProductName = prompt("Enter the Product Name");
+let CustomerName= prompt("Enter The Customer Name");
+let Price =1500;
+
+console.log ("WebSideNAme :", WebSideNAme);
+console.log ("ProductName :",ProductName);
+console.log ("CustomerName :",CustomerName);
+console.log ("Price :",Price);
+
+//Console Method Tasks
+// Task 11;  success message using console.log().
+console.log("Registration Successful!");
+
+//Task 12; error using console.error().
+console.error("Payment Failed!");
+
+//Task 13; warning using console.warn().
+console.warn("Battery Low");
+
+
+//Task 14; some values and then clear the console using console.clear().
+console.log("Loading...");
+console.log("Welcome");
+
+console.clear();
+
+//Task 15; Alert, Confirm & Prompt Tasks
+alert("Welcome to Amazon");
+
+//Task 16; Do you want to logout?  using confirm().
+let logout = confirm("Do you want to logout?");
+console.log(logout);
+
+//Task 17; Ask the user's city using prompt().
+let city = prompt("Enter Your City");
+console.log("Your City :", city);
+
+//Task 18; Variable Practice
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+
+console.log(product);
+
+
+//Task 19; create
+let salary = 25000;
+salary = 30000;
+
+console.log(salary);
+
+//Task 20;create
+const Company ="Google";
+console.log("Company :",Company);
