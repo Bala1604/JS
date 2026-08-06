9a59ae465316e6f5082c45fcfacffc986a752fd9
Bala1604 <balasm1504@gmail.com>
2026-08-06 12:23:37 +0530

    commit

---
+++ fun.js
@@ -0,0 +1,102 @@
+// named function
+
+function name(){
+    console.log("hello");
+}
+name()
+
+// return statment
+function add(a,b){
+    return a*b;
+}
+let res = add(10,20);
+console.log("res:",res );
+
+// anonymous function
+var annonymous = function(parameter){
+    console.log(parameter);
+ }
+
+annonymous("annonymous function")
+
+// with para
+const sum = function(a,b){
+    console.log(a*b);
+}
+sum(30,30)
+
+// with return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function function1(){
+// //     console.log("higher order");
+
+// // }
+
+// // function function2(){
+// //     console.log("callback function");
+
+// // }
+
+
+// // function1(function2())
+
+
+
+
+// // (function(parameter){
+// //     console.log(parameter);
+
+// // })("argument")
+
+
+// // (function(product,discount){
+
+// //     alert(`now sales going on , ${product} dicount ${discount}%`)
+
+// // })("phant",50)
+ 
+
+
+// function* gen(){
+//     yield "10% cashback"
+//     yield "20% cashback"
+//     yield "30% cashback"
+//     yield "40% cashback"
+//     yield "try again"
+//     yield "100% cashback"
+// }
+
+
+// let allGen = gen()
+
+// console.log(allGen.next());
+// console.log(allGen.next());
+// console.log(allGen.next());
+// console.log(allGen.next());
+// console.log(allGen.next());
+// console.log(allGen.next());
+
+
+// for(let a of allGen){
+//     console.log(a);
+
+// }
+
+
+
